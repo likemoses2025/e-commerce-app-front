6a0963524cfc2dcb7421ebb4cce3e2b9318464d2
likemoses2025 <mose233@gmail.com>
2023-03-03 23:18:11 +0900

    Create Home Screen

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Main from "./Main";
 
-export default function App() {
-  return (
-    <View>
-      <Main />
-    </View>
-  );
-}
+const App = () => {
+  return <Main />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
--- Main.jsx
+++ Main.jsx
@@ -1,26 +1,17 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  StatusBar,
-  Platform,
-} from "react-native";
 import React from "react";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./screens/Home ";
-import Login from "./screens/Login";
+import Home from "./screens/Home";
+
+const Stack = createNativeStackNavigator();
 
 const Main = () => {
-  const Stack = createNativeStackNavigator();
-
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="login">
+      <Stack.Navigator>
         <Stack.Group>
           <Stack.Screen name="home" component={Home} />
-          <Stack.Screen name="login" component={Login} />
         </Stack.Group>
       </Stack.Navigator>
     </NavigationContainer>
